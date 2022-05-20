c218201821480df3bcfee8e6631ac8200786993f
FiBoot <fiboot89@gmail.com>
2022-05-20 15:35:33 +0200

    all ranking

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
 // ==UserScript==
 // @name         pokemonclick addin
 // @namespace    http://tampermonkey.net/
-// @version      2.5
+// @version      3.0
 // @author       FiBoot
 // @match        https://www.pokeclicker.com/
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=pokeclicker.com
 // @grant        none
 // @require      https://raw.githubusercontent.com/FiBoot/pokeclicker-addin/main/script.js
 // ==/UserScript==
+
+// RANKING
+// https://docs.google.com/spreadsheets/d/1rgK21U5RtCrozSgOWSBlOW9VS0j_nm1UmESSHVstHEU/edit?usp=sharing
 
 // TIMESPAN -> CLICK PER SECONDS
 //  1       -> 1000
