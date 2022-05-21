1dcea848bbbac18969730e962fd02640525543f7
FiBoot <fiboot89@gmail.com>
2022-05-22 00:38:35 +0200

    protein link

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         pokemonclick addin
 // @namespace    http://tampermonkey.net/
-// @version      5.2
+// @version      5.3
 // @author       FiBoot
 // @match        https://www.pokeclicker.com/
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=pokeclicker.com
@@ -10,8 +10,8 @@
 // @require      https://raw.githubusercontent.com/FiBoot/pokeclicker-addin/main/script.js
 // ==/UserScript==
 
+// POKECLICK WIKI
+// https://pokeclicker.miraheze.org/wiki/Protein#Alola
+
 // RANKING SHEET
 // https://docs.google.com/spreadsheets/d/1rgK21U5RtCrozSgOWSBlOW9VS0j_nm1UmESSHVstHEU/edit?usp=sharing
-
-// POKECLICK WIKI
-// https://pokeclicker.miraheze.org/wiki
