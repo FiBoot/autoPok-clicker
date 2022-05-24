e4acb71c7a93176e8dacd123b2e237db9f5ee181
FiBoot <fiboot89@gmail.com>
2022-05-24 22:04:00 +0200

    fix dungeon ver

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         pokemonclick addin
 // @namespace    http://tampermonkey.net/
-// @version      6.5
+// @version      6.7
 // @author       FiBoot
 // @match        https://www.pokeclicker.com/
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=pokeclicker.com
